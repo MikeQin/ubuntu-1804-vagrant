a14dffec84d4a43fc52f572c7509a9f8d5a0e1e8
Michael Qin <mike.qin@gmail.com>
2019-02-28 01:10:46 -0500

    Update user-config.sh

--- user-config.sh
+++ user-config.sh
@@ -10,13 +10,5 @@ export PATH=$PATH:.
 EOF
 source $HOME/.profile
 
-# Get jenkins server
-#wget -q http://mirrors.jenkins.io/war-stable/latest/jenkins.war -O $HOME/jenkins.war
-#chmod 755 $HOME/jenkins.war
-
-# Create startup file
-#echo 'java -jar jenkins.war --httpPort=8081' > $HOME/jenkins-server.sh
-#chmod 755 $HOME/jenkins-server.sh
-
-# Change owner for .dbus dir
-sudo chown -R $USER:$USER $HOME/.dbus
+# Add more user config below
+# ...
